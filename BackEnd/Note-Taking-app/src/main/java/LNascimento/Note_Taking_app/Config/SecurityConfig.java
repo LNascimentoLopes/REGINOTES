c0be14521c645e64b9eb55c9490d0e189c1c579e
LNascimentoLopes <lucas,nascimento3224@gmail.com>
2026-05-27 15:07:50 -0300

    Fix: cors config

--- BackEnd/Note-Taking-app/src/main/java/LNascimento/Note_Taking_app/Config/SecurityConfig.java
+++ BackEnd/Note-Taking-app/src/main/java/LNascimento/Note_Taking_app/Config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -69,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("https://reginotes.vercel.app","http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
